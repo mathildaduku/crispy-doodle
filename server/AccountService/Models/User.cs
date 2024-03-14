07af9a06cc3440239d0eca21dace0897c3d439f9
Jerry Abadi <abadijerry44@gmail.com>
2024-03-14 09:39:37 +0100

    lol

---
+++ server/AccountService/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AccountService.Models
+{
+    public class User : IdentityUser
+    {
+        public Guid Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
