eb855afd500fe3b4d98575316d38995fd669a585
Duku Aghogho Mathida <mathildaduku@gmail.com>
2024-03-18 12:13:31 +0100

    add automapper

---
+++ server/AccountService/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿/*using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public UserController()
+        {
+            
+        }
+        [HttpGet()]
+    }
+}
+*/
---
+++ server/AccountService/Dto/Response/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AccountService.Dto.Response
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+    }
+}
