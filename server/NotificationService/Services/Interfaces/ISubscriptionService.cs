f5e08ecc1f0178853f9b92a839d93394a102fe12
Duku Aghogho Mathida <104989791+mathildaduku@users.noreply.github.com>
2024-04-03 03:02:10 +0000

    add delete subscription and get subscribers function

--- server/NotificationService/Services/Interfaces/ISubscriptionService.cs
+++ server/NotificationService/Services/Interfaces/ISubscriptionService.cs
@@ -5,4 +5,6 @@ namespace NotificationService;
 public interface ISubscriptionService
 {
     public Task AddSubscriptionAsync(Subscription subscription);
+    public Task DeleteSubscriptionAsync(Subscription subscription);
+   public Task<List<Subscription>> GetUserSubscribers(Guid userId);
 }
